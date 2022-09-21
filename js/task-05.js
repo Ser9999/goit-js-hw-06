473676d502fa359583283ddd490d02d41b0340b4
Ser9999 <mosiuksergiy@gmail.com>
2022-09-21 14:29:17 +0300

    5

--- js/task-05.js
+++ js/task-05.js
@@ -3,18 +3,15 @@ const myInput = {
   inputEl: document.querySelector("#name-input"),
   mySpan: document.querySelector("#name-output"),
 };
-
 myInput.inputEl.addEventListener("input", (event) => {
   if (onEmptyString(event.currentTarget.value)) {
     return (myInput.mySpan.textContent = "Anonymous");
   }
-  refs.mySpan.textContent = event.currentTarget.value;
+  myInput.mySpan.textContent = event.currentTarget.value;
 });
 
 function onEmptyString(str) {
   return !str || 0 === str.length;
 }
-
-
 
 
